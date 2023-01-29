eef32b666ed6608292464f62ac05ea980069fedd
Denys Demianov <demydeni@gmail.com>
2023-01-29 19:47:35 +0100

    Update backend api

--- app/src/main/java/com/payrem/backend/api/TransformationUtils.kt
+++ app/src/main/java/com/payrem/backend/api/TransformationUtils.kt
@@ -4,39 +4,31 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.payrem.backend.entities.*
 
+fun jsonToApplicationUser(jsonBody: String): ApplicationUser {
+    return Gson().fromJson(jsonBody, ApplicationUser::class.java)
+}
+
 fun jsonArrayToApplicationUsers(jsonBody: String): List<ApplicationUser> {
     val typeToken = object : TypeToken<List<ApplicationUser>>() {}.type
     return Gson().fromJson(jsonBody, typeToken)
 }
 
-fun jsonArrayToExpenses(jsonBody: String): ArrayList<Reminder> {
-    val typeToken = object : TypeToken<ArrayList<Reminder>>() {}.type
+
+fun jsonToGroup(jsonBody: String): Group {
+    return Gson().fromJson(jsonBody, Group::class.java)
+}
+
+fun jsonArrayToGroups(jsonBody: String): List<Group> {
+    val typeToken = object : TypeToken<List<Group>>() {}.type
     return Gson().fromJson(jsonBody, typeToken)
 }
 
-fun jsonArrayToExpenseGroups(jsonBody: String): List<ReminderGroup> {
-    val typeToken = object : TypeToken<List<ReminderGroup>>() {}.type
-    return Gson().fromJson(jsonBody, typeToken)
-}
 
-fun jsonToApplicationUser(jsonBody: String): ApplicationUser {
-    return Gson().fromJson(jsonBody, ApplicationUser::class.java)
-}
-
-fun jsonToExpense(jsonBody: String): Reminder {
+fun jsonToReminder(jsonBody: String): Reminder {
     return Gson().fromJson(jsonBody, Reminder::class.java)
 }
 
-fun jsonToExpenseGroup(jsonBody: String): ReminderGroup {
-    return Gson().fromJson(jsonBody, ReminderGroup::class.java)
-}
-
-
-fun jsonToLoginAndPassword(jsonBody: String): LoginAndPassword {
-    return Gson().fromJson(jsonBody, LoginAndPassword::class.java)
-}
-
-fun jsonArrayToNotification(jsonBody: String): List<Notification> {
-    val typeToken = object : TypeToken<List<Notification>>() {}.type
+fun jsonArrayToReminders(jsonBody: String): ArrayList<Reminder> {
+    val typeToken = object : TypeToken<ArrayList<Reminder>>() {}.type
     return Gson().fromJson(jsonBody, typeToken)
 }
